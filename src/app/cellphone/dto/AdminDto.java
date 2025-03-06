d5a055afc3df14efd4cdff5f33a5c08879b587d9
Jang Seung Beom <wkdtmf357@gmail.com>
2025-03-06 11:23:49 +0900

    feat: Added Dto for Admin

---
+++ src/app/cellphone/dto/AdminDto.java
@@ -0,0 +1,41 @@
+package app.cellphone.dto;
+
+public class AdminDto {
+	private int adminId;
+	private String username;
+	private String password;
+	
+	public AdminDto() {}
+
+	public AdminDto(int adminId, String username, String password) {
+		super();
+		this.adminId = adminId;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return adminId;
+	}
+
+	public void setUserId(int userId) {
+		this.adminId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
